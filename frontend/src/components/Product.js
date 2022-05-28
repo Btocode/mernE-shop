b34d71d242d5ebfec88cb9f8e7f55dce61fd407a
Afsan Saeed <afsansaeed2017@gmail.com>
2022-05-28 18:39:05 +0600

    Product Rating

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import Rating from '../components/Rating';
 
 const Product = ({ product }) => {
   return (
@@ -14,12 +15,15 @@ const Product = ({ product }) => {
               <strong>{product.name}</strong>
             </Card.Title>
           </a>
-          <Card.Text as="div">
-            <div className='my-3'>
-              {product.rating}* from {product.numReviews} reviews.
-            </div>
+
+          <Card.Text as='div'>
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
           </Card.Text>
-          <Card.Text as = "h3">${product.price}</Card.Text>
+
+          <Card.Text as='h3'>${product.price}</Card.Text>
         </Card.Body>
       </Card>
     </>
